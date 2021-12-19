977bcb096098f042d83d935714ec0cea95426047
nika kotrikadze <nikakotrikadze12@gmail.com>
2021-12-19 14:46:47 +0400

    nicecommit

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const description = document.querySelector(".description");
 const game = document.querySelector(".game");
 const masterDiv = document.querySelector(".masterDiv");
-
-function Start() {
-  description.style.display = "flex";
-  game.style.display = "flex";
-  masterDiv.style.display = "none";
-}
+const mazeBorder = document.querySelector(".maze-border");
 
 //maze levels
 const levelOne = document.querySelector(".level-one");
@@ -19,7 +14,39 @@ const uiMessage = document.querySelector(".ui-message");
 
 //end game
 const scaryPic = document.querySelector(".scary-pic");
-const goodEnd = document.querySelector(".goodEnd");
+const screamSound = document.querySelector(".screamSound");
+
+//start game
+const timer = document.querySelector(".timer");
+const darudeSandstorm = document.querySelector(".darudeSandstorm");
+function Start() {
+  description.style.display = "flex";
+  game.style.display = "flex";
+  masterDiv.style.display = "none";
+  darudeSandstorm.play();
+  timer.style.display = "block";
+  startTimer();
+}
+
+//timer
+function startTimer() {
+  var counter = 60;
+  setInterval(function () {
+    counter--;
+    if (counter >= 0) {
+      timer.innerHTML = counter + " seconds";
+    }
+    if (counter === 0) {
+      // alert("Sorry, Out of Time");
+      masterDiv.style.display = "flex";
+      darudeSandstorm.pause();
+      levelOne.style.display = "flex";
+      levelTwo.style.display = "none";
+      levelThree.style.display = "none";
+      clearInterval(counter);
+    }
+  }, 1000);
+}
 
 //when collision happens
 const shejaxeba = (value) => {
@@ -37,7 +64,8 @@ const shejaxeba = (value) => {
     uiMessage.textContent = "Last One.. You got This";
   }
   if (value === "end-game") {
-    goodEnd.play();
+    darudeSandstorm.pause();
+    screamSound.play();
     scaryPic.style.display = "block";
     document.body.style.background = "black";
   }
